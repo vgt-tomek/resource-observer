d65131e5e18513997169f50358821fb8d24a6105
VGT <tomek@vgtworld.pl>
2015-11-20 20:41:00 +0100

    Create schema for resources.

---
+++ schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE resources (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  url varchar(255) NOT NULL,
+  active boolean NOT NULL,
+  check_interval INT UNSIGNED NOT NULL,
+  created_at DATETIME NOT NULL,
+  UNIQUE(name),
+  UNIQUE(url)
+);
+
+CREATE TABLE resource_observers (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  resource_id INT UNSIGNED NOT NULL,
+  email varchar(255) NOT NULL,
+  UNIQUE(resource_id, email),
+  CONSTRAINT FOREIGN KEY resource_observers_resource_id_fkey (resource_id) REFERENCES resources(id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
