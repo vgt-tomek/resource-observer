c7565b18d4658bf6d565bcd810fc76f3ec2c6352
VGT <tomek@vgtworld.pl>
2015-11-28 22:28:18 +0100

    Create class with property names constants. Add constant for sender e-mail address for notifications.

---
+++ src/main/java/pl/vgtworld/resourceobserver/core/PropertyNames.java
@@ -0,0 +1,10 @@
+package pl.vgtworld.resourceobserver.core;
+
+public final class PropertyNames {
+
+	public static final String MAIL_SESSION_SENDER_ADDRESS = "pl.vgtworld.resourceobserver.mail.from";
+
+	private PropertyNames() {
+	}
+
+}
